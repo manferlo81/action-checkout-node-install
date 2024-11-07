b23f9c9cf0127b82a7b64c4c61ae664d3d722e02
Manuel Fernández <manferlo81@gmail.com>
2024-11-07 13:26:43 +0700

    chore: Release only full tag

--- scripts/extract-major-tag.js
+++
@@ -1,8 +0,0 @@
-const inputTag = process.argv[2];
-
-if (!inputTag) throw new Error('No input');
-if (!inputTag.startsWith('v')) throw new Error('Invalid input tag');
-
-const majorTag = inputTag.split('.')[0];
-
-console.log(majorTag);
